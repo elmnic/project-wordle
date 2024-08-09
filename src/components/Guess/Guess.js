4a0004214fd9eb0bea8bf9a3290b6f93750d62e1
Niclas Elmäng <niclas.elmang@pagero.com>
2024-08-09 12:11:27 +0200

    Exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 import { range } from '../../utils'
 
-function Guess({ value }) {
+function Guess({ value, status }) {
   return (
     <p className="guess">
-      {range(5).map((letterIndex) => (
-        <span key={letterIndex} className="cell">
-          {!!value ? value.split('')[letterIndex] : null}
-        </span>
-      ))}
+      {range(5).map((letterIndex) => {
+        return (
+          <span
+            key={letterIndex}
+            className={`cell ${!!status ? status[letterIndex].status : null}`}
+          >
+            {!!value ? value.split('')[letterIndex] : null}
+          </span>
+        )
+      })}
     </p>
   )
 }
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -2,13 +2,25 @@ import React from 'react'
 import { range } from '../../utils'
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 import Guess from '../Guess'
+import { checkGuess } from '../../game-helpers'
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
-      {range(NUM_OF_GUESSES_ALLOWED).map((rowIndex) => (
-        <Guess key={rowIndex} value={guesses.length >= rowIndex ? guesses[rowIndex] : null} />
-      ))}
+      {range(NUM_OF_GUESSES_ALLOWED).map((rowIndex) => {
+        const currentGuess =
+          guesses.length >= rowIndex ? guesses[rowIndex] : null
+        const currentGuessStatus = currentGuess
+          ? checkGuess(currentGuess, answer)
+          : null
+        return (
+          <Guess
+            key={rowIndex}
+            value={currentGuess}
+            status={currentGuessStatus}
+          />
+        )
+      })}
     </div>
   )
 }
