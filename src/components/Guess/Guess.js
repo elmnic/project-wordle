203122279d7fd71cc63e09dacdd3244a86337025
Niclas Elmäng <niclas.elmang@pagero.com>
2024-08-09 12:43:26 +0200

    Exercise 4 small refactor

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -6,10 +6,12 @@ function Guess({ value, status }) {
   return (
     <p className="guess">
       {range(5).map((letterIndex) => {
-        const statusClass = !!status ? status[letterIndex].status : null
+        const statusClass = !!status
+          ? `cell ${status[letterIndex].status}`
+          : 'cell'
         const letter = !!value ? value.split('')[letterIndex] : null
         return (
-          <span key={letterIndex} className={`cell ${statusClass}`}>
+          <span key={letterIndex} className={statusClass}>
             {letter}
           </span>
         )
